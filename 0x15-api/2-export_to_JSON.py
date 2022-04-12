4fe7398573aae946523cf6343af710fa5c18c2cc
Robi000 <88291984+Robi000@users.noreply.github.com>
2022-04-13 02:33:54 +0300

    Update 2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
 """getting data from an api
 """
 
@@ -6,24 +8,31 @@ import requests
 from sys import argv
 import json
 
-user = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(
-    argv[1])).json()
-todo = requests.get(
-    "https://jsonplaceholder.typicode.com/users/{}/todos".format(
-        argv[1])).json()
 
-final = {}
-list_inner = []
+def main():
+    """this is main funtion to get run 
+    """
 
-for x in todo:
-    inner_todo = {}
-    inner_todo["task"] = x["title"]
-    inner_todo["completed"] = x["completed"]
-    inner_todo["username"] = user["username"]
-    list_inner.append(inner_todo)
+    user = \
+        requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(argv[1])).json()
+    todo = \
+        requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'.format(argv[1])).json()
 
-final[str(user["id"])] = list_inner
+    final = {}
+    list_inner = []
 
-with open("USER_ID.json", "w") as file:
-    json.dump(final, file, indent=2)
+    for x in todo:
+        inner_todo = {}
+        inner_todo['task'] = x['title']
+        inner_todo['completed'] = x['completed']
+        inner_todo['username'] = user['username']
+        list_inner.append(inner_todo)
 
+    final[str(user['id'])] = list_inner
+
+    with open('USER_ID.json', 'w') as file:
+        json.dump(final, file, indent=2)
+
+
+if __name__ == '__main__':
+    main()
