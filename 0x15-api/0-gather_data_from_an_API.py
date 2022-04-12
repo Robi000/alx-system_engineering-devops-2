2351c29ecea4b089e738f7d5b1335334a2eaa9a7
Robi000 <88291984+Robi000@users.noreply.github.com>
2022-04-13 02:09:29 +0300

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -13,14 +13,21 @@ todo = \
     requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'.format(argv[1])).json()  # this is where we find to do list
 
 countx = 0
-for y in todo:  # this is where we itrate to do list
+for y in todo: 
+    """this wis well documentd file 
+    """
+    # this is where we itrate to do list
     if y['completed'] == True:
         countx += 1
-
+"""this wis well documentd file 
+    """
 # this is where we print the user thing
 
 print ('Employee {} is done with tasks({}/{}):'.format(user['name'],
         countx, len(todo)))
+
 for y in todo:
+    """this wis well documentd file 
+    """
     if y['completed'] == True:
         print ('\t ', y['title'])
