0f1b9fe7c5a11ab549784d1fddcb8e17ae617611
Robi000 <88291984+Robi000@users.noreply.github.com>
2022-04-13 02:15:40 +0300

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,20 +1,32 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
 """getting data from an api
+    Records all tasks that are owned by this employee
+    Format must be: "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+    File name must be: USER_ID.csv
     """
 
 import requests
 from sys import argv
 
-user = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(
-    argv[1])).json()
-todo = requests.get(
-    "https://jsonplaceholder.typicode.com/users/{}/todos".format(
-        argv[1])).json()
 
-final = ""
-for x in todo:
-    final = final + "\"{}\",\"{}\",\"{}\"\n".format(x["userId"],
-                                                    x["completed"], x["title"])
+def main():
+    """this tis the main function"""
 
-with open("USER_ID.csv", "w") as f:
-    f.write(final)
+    user = \
+        requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(argv[1])).json()
+    todo = \
+        requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'.format(argv[1])).json()
+
+    final = ''
+    for x in todo:
+        final = final + '"{}","{}","{}"\n'.format(x['userId'],
+                x['completed'], x['title'])
+
+    with open('USER_ID.csv', 'w') as f:
+        f.write(final)
+
+
+if __name__ == '__main__':
+    main()
