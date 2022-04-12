0b70157956c1b0f5d6f66c420983548503492c69
Robi000 <88291984+Robi000@users.noreply.github.com>
2022-04-13 02:12:03 +0300

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,34 +1,31 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
-import requests
-from sys import argv
-
 '''Module 0-gather_data_from_an_API
 using a REST API, for a given employee ID, returns information
 about his/her TODO list progress.'''
+import requests
+from sys import argv
 
-user = \
-    requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(argv[1])).json()  # this is where we find user interface
-todo = \
-    requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'.format(argv[1])).json()  # this is where we find to do list
 
-countx = 0
-for y in todo: 
-    """this wis well documentd file 
-    """
-    # this is where we itrate to do list
-    if y['completed'] == True:
-        countx += 1
-"""this wis well documentd file 
-    """
-# this is where we print the user thing
+def main():
+    '''Program starts here'''
+    user_id = argv[1]
+    user_name = requests.get(
+        'https://jsonplaceholder.typicode.com/users/' +
+        user_id).json().get('name')
+    # print("USER: ", user)
 
-print ('Employee {} is done with tasks({}/{}):'.format(user['name'],
-        countx, len(todo)))
+    completed_tasks = requests.get(
+        'https://jsonplaceholder.typicode.com/todos',
+        params={'userId': user_id, 'completed': 'true'}).json()
+    all_tasks = requests.get(
+        'https://jsonplaceholder.typicode.com/todos',
+        params={'userId': user_id}).json()
 
-for y in todo:
-    """this wis well documentd file 
-    """
-    if y['completed'] == True:
-        print ('\t ', y['title'])
+    print("Employee", user_name, "is done with tasks(" +
+          str(len(completed_tasks)) + "/" + str(len(all_tasks)) + "):")
+    for i in completed_tasks:
+        print("\t " + i['title'])
+
+
+if __name__ == '__main__':
+    main()
