74eafee74d1aacca0c79942798b015b1faa0e3c2
Robi000 <88291984+Robi000@users.noreply.github.com>
2022-04-13 03:17:02 +0300

    Update 3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,41 +1,34 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
 """getting data from an api
 """
 
 import json
 import requests
 
-user = requests.get("https://jsonplaceholder.typicode.com/users").json()
-"""{ "USER_ID": [ {"username": "USERNAME", "task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS}, 
-    {"username": "USERNAME", "task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS}, ... ], 
-    "USER_ID": [ {"username": "USERNAME", "task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS}, ...... }}
-    """
+user = requests.get('https://jsonplaceholder.typicode.com/users').json()
 
 final = {}  # TODO: final [str(user[id])] = list_inner
-"""  [ {"username": "USERNAME", "task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS}, 
-    {"username": "USERNAME", "task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS}, ... ]
-    """
 
 list_inner = []  # TODO: list_inner.append(todo_dict)
-"""
-"username": "USERNAME", "task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS
-"""
 
-todo_dict = {
-}  # TODO: todo_dict["username"], todo_dict["task"], todo_dict["completed"]
+todo_dict = {}
+
+   # TODO: todo_dict["username"], todo_dict["task"], todo_dict["completed"]
 
 for x in user:
     list_inner = []
-    todo = requests.get(
-        "https://jsonplaceholder.typicode.com/users/{}/todos".format(
-            x["id"])).json()
+    todo = \
+        requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'.format(x['id'
+                     ])).json()
     for y in todo:
         todo_dict = {}
-        todo_dict["username"] = x["username"]
-        todo_dict["task"] = y["title"]
-        todo_dict["completed"] = y["completed"]
+        todo_dict['username'] = x['username']
+        todo_dict['task'] = y['title']
+        todo_dict['completed'] = y['completed']
         list_inner.append(todo_dict)
-    final[str(x["id"])] = list_inner
+    final[str(x['id'])] = list_inner
 
-with open("todo_all_employees.json", "w") as file:
+with open('todo_all_employees.json', 'w') as file:
     json.dump(final, file, indent=2)
