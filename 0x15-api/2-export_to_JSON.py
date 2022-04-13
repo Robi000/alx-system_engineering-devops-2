150787fa9be8eb0730b8afcb2c5653fdaebe5040
Robi000 <88291984+Robi000@users.noreply.github.com>
2022-04-13 03:02:28 +0300

    Update 2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,38 +1,36 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
+"""getting data from an api
+"""
 
-'''Module 1-export_to_CSV
-Exports data got from API to CSV'''
-
-import json
 import requests
 from sys import argv
+import json
 
 
 def main():
-    '''Program starts here'''
-
-    user = \
-        requests.get('https://jsonplaceholder.typicode.com\
-        /users/{}'.format(argv[1])).json()
-    todo = \
-        requests.get('https://jsonplaceholder.typicode.com/\
-        users/{}/todos'.format(argv[1])).json()
+    """this is main funtion to get run 
+    """
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(
+        argv[1])).json()
+    todo = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}/todos".format(
+            argv[1])).json()
 
     final = {}
     list_inner = []
 
     for x in todo:
         inner_todo = {}
-        inner_todo['task'] = x['title']
-        inner_todo['completed'] = x['completed']
-        inner_todo['username'] = user['username']
+        inner_todo["task"] = x["title"]
+        inner_todo["completed"] = x["completed"]
+        inner_todo["username"] = user["username"]
         list_inner.append(inner_todo)
 
-    final[str(user['id'])] = list_inner
+    final[str(user["id"])] = list_inner
 
-    with open('USER_ID.json', 'w') as file:
+    with open(str(user["id"]) + ".json", "w") as file:
         json.dump(final, file, indent=2)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
